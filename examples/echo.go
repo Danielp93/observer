2f80096832ddad7c993424181d8bc1473eab63ae
danielp93 <danielpot93@gmail.com>
2021-12-03 19:11:51 +0100

    fix: shuffling around

---
+++ examples/echo.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/danielp93/observer"
+)
+
+func main() {
+	// Create Notifier
+	n := observer.NewDefaultNotifier
+	if n == nil {
+		return
+	}
+
+	// Create Listener from Listenerfunc (func(Message))
+	// This is a simple Listener that will print out the message
+	// If message is a Simplemessage it also will print the type and timestamp
+	l := observer.ListenerFunc(func(message observer.Message) {
+		switch m := message.(type) {
+		case *observer.SimpleMessage:
+			fmt.Printf("Received Message [%s] %s @%v\n", m.Type, m.Message, m.Timestamp)
+		default:
+			fmt.Println(message)
+		}
+	})
+
+	// Subscribe Listener to Notifier
+	n.Subscribe(l)
+
+	m := observer.NewMessage("ECHO", "Hello from the other side!")
+
+	n.Notify(m)
+}
